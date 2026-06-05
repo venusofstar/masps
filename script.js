c7c67f4f68e1b982d430501d61a544857c1ffc9e
venusofstar <sumidomikeangelo24@gmail.com>
2026-06-06 07:01:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ const channels = [
   service: "F1",
   logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMs-qvMbBce47vo45QG1PlOjKTgyfWeid3uQ&s",
   page: "f1.html",
-  date: "2026-06-7",
+  date: "2026-06-6",
   startTime: "10:00 PM",
   endTime: "2:59 AM"
    },
