6d1f52fdc6b6125aa5d9e5fa632bf48419f2bc74
venusofstar <sumidomikeangelo24@gmail.com>
2026-06-21 09:27:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -319,7 +319,7 @@ const channels = [
   "service": "FIFA World Cup",
   "logo": "https://statics.foxsports.com/www.foxsports.com/content/uploads/2023/05/IMG_6227.png",
   "page": "fifa1.html",
-  "date": "2026-08-21",
+  "date": "2026-06-21",
   "startTime": "8:30 AM",
   "endTime": "11:00 AM"
 },
