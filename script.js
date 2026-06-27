ad83779b897bfdd564c3c25bc7f159f7e5af901e
venusofstar <sumidomikeangelo24@gmail.com>
2026-06-28 00:18:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -38,9 +38,9 @@ const channels = [
   service: "UFC",
   logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/1280px-UFC_Logo.svg.png",
   page: "ufc1.html",
-  date: "2026-06-27",
-  startTime: "8:00 PM",
-  endTime: "11:59 PM"
+  date: "2026-06-28",
+  startTime: "12:00 AM",
+  endTime: "3:59 AM"
 },
 {
   title: "UFC - PANCRASE 363",
