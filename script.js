a623bbdab2f9261615d8b6fb77386923b5e39019
venusofstar <sumidomikeangelo24@gmail.com>
2026-06-29 18:25:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -69,61 +69,6 @@ const channels = [
 "endTime": "5:59 PM"
 },
 {
-"title": "British Grand Prix - Practice 1",
-"category": "F1",
-"service": "F1",
-"logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMs-qvMbBce47vo45QG1PlOjKTgyfWeid3uQ&s",
-"page": "f1.html",
-"date": "2026-07-03",
-"endDate": "2026-07-03",
-"startTime": "7:30 PM",
-"endTime": "11:59 PM"
-},
-{
-"title": "British Grand Prix - Sprint Qualifying",
-"category": "F1",
-"service": "F1",
-"logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMs-qvMbBce47vo45QG1PlOjKTgyfWeid3uQ&s",
-"page": "f1.html",
-"date": "2026-07-03",
-"endDate": "2026-07-04",
-"startTime": "11:30 PM",
-"endTime": "3:59 AM"
-},
-{
-"title": "British Grand Prix - Sprint",
-"category": "F1",
-"service": "F1",
-"logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMs-qvMbBce47vo45QG1PlOjKTgyfWeid3uQ&s",
-"page": "f1.html",
-"date": "2026-07-04",
-"endDate": "2026-07-04",
-"startTime": "7:00 PM",
-"endTime": "11:59 PM"
-},
-{
-"title": "British Grand Prix - Qualifying",
-"category": "F1",
-"service": "F1",
-"logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMs-qvMbBce47vo45QG1PlOjKTgyfWeid3uQ&s",
-"page": "f1.html",
-"date": "2026-07-04",
-"endDate": "2026-07-04",
-"startTime": "10:00 PM",
-"endTime": "10:59 PM"
-},
-{
-"title": "British Grand Prix - Race",
-"category": "F1",
-"service": "F1",
-"logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMs-qvMbBce47vo45QG1PlOjKTgyfWeid3uQ&s",
-"page": "f1.html",
-"date": "2026-07-05",
-"endDate": "2026-07-05",
-"startTime": "9:00 PM",
-"endTime": "11:59 PM"
-},
-{
 "title": "Croatia vs. Ghana",
 "category": "FIFA World Cup",
 "service": "FIFA World Cup",
@@ -419,7 +364,62 @@ const channels = [
 "endDate": "2026-06-27",
 "startTime": "11:00 AM",
 "endTime": "1:29 PM"
-}
+},
+  {
+ "title": "British Grand Prix - Practice 1",
+"category": "F1",
+"service": "F1",
+"logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMs-qvMbBce47vo45QG1PlOjKTgyfWeid3uQ&s",
+"page": "f1.html",
+"date": "2026-07-03",
+"endDate": "2026-07-03",
+"startTime": "7:30 PM",
+"endTime": "11:59 PM"
+},
+{
+"title": "British Grand Prix - Sprint Qualifying",
+"category": "F1",
+"service": "F1",
+"logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMs-qvMbBce47vo45QG1PlOjKTgyfWeid3uQ&s",
+"page": "f1.html",
+"date": "2026-07-03",
+"endDate": "2026-07-04",
+"startTime": "11:30 PM",
+"endTime": "3:59 AM"
+},
+{
+"title": "British Grand Prix - Sprint",
+"category": "F1",
+"service": "F1",
+"logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMs-qvMbBce47vo45QG1PlOjKTgyfWeid3uQ&s",
+"page": "f1.html",
+"date": "2026-07-04",
+"endDate": "2026-07-04",
+"startTime": "7:00 PM",
+"endTime": "11:59 PM"
+},
+{
+"title": "British Grand Prix - Qualifying",
+"category": "F1",
+"service": "F1",
+"logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMs-qvMbBce47vo45QG1PlOjKTgyfWeid3uQ&s",
+"page": "f1.html",
+"date": "2026-07-04",
+"endDate": "2026-07-04",
+"startTime": "10:00 PM",
+"endTime": "10:59 PM"
+},
+{
+"title": "British Grand Prix - Race",
+"category": "F1",
+"service": "F1",
+"logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMs-qvMbBce47vo45QG1PlOjKTgyfWeid3uQ&s",
+"page": "f1.html",
+"date": "2026-07-05",
+"endDate": "2026-07-05",
+"startTime": "9:00 PM",
+"endTime": "11:59 PM"
+}  
 ];
 
 /* ------------------------------------------------
